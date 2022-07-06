69c83ec727402c207ecff1ae81fbd153162ca4b3
Andrii.Baklanov <andrii.baklanov@anvileight.com>
2022-07-06 19:01:09 +0300

    implement randomness

--- contracts/BullAndBear.sol
+++ contracts/BullAndBear.sol
@@ -15,14 +15,26 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // ./interfaces/KeeperCompatibleInterface.sol
 import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 // Dev imports
 import "hardhat/console.sol";
 
-contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibleInterface, Ownable  {
+contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatibleInterface, VRFConsumerBaseV2 {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    AggregatorV3Interface public pricefeed;
+    AggregatorV3Interface public priceFeed;
+
+    // VRF
+    VRFCoordinatorV2Interface public COORDINATOR;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    uint32 public callbackGasLimit = 500000; // set higher as fulfillRandomWords is doing a LOT of heavy lifting.
+    uint64 public s_subscriptionId;
+    bytes32 keyhash =  0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc; // keyhash, see for Rinkeby https://docs.chain.link/docs/vrf-contracts/#rinkeby-testnet
     
     /**
     * Use an interval in seconds and a timestamp to slow execution of Upkeep
@@ -31,6 +43,9 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
     uint public lastTimeStamp;
 
     int256 public currentPrice;
+
+    enum MarketTrend{BULL, BEAR} // Create Enum
+    MarketTrend public currentMarketTrend = MarketTrend.BULL; 
     
     // IPFS URIs for the dynamic nft graphics/metadata.
     // NOTE: These connect to my IPFS Companion node.
@@ -50,7 +65,7 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
 
     // For testing with the mock on Rinkeby, pass in 10(seconds) for `updateInterval` and the address of my 
     // deployed  MockPriceFeed.sol contract (0xD753A1c190091368EaC67bbF3Ee5bAEd265aC420).
-    constructor(uint updateInterval, address _pricefeed) ERC721("Bull&Bear", "BBTK") {
+    constructor(uint updateInterval, address _pricefeed, address _vrfCoordinator) ERC721("Bull&Bear", "BBTK") VRFConsumerBaseV2(_vrfCoordinator) {
         // Set the keeper update interval
         interval = updateInterval; 
         lastTimeStamp = block.timestamp;  //  seconds since unix epoch
@@ -58,10 +73,12 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
         // set the price feed address to
         // BTC/USD Price Feed Contract Address on Rinkeby: https://rinkeby.etherscan.io/address/0xECe365B379E1dD183B20fc5f022230C044d51404
         // or the MockPriceFeed Contract
-        pricefeed = AggregatorV3Interface(_pricefeed); // To pass in the mock
-
+        priceFeed = AggregatorV3Interface(_pricefeed); // To pass in the mock
         // set the price for the chosen currency pair.
         currentPrice = getLatestPrice();
+
+        // VRF
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);  
     }
 
     function safeMint(address to) public  {
@@ -81,6 +98,7 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
         console.log("DONE!!! minted token ", tokenId, " and assigned token url: ", defaultUri);
     }
 
+    //https://docs.chain.link/docs/chainlink-keepers/compatible-contracts/
     function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /*performData */) {
          upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
 
@@ -99,15 +117,17 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
 
             if (latestPrice < currentPrice) {
                 // bear
-                console.log("ITS BEAR TIME");
-                updateAllTokenUris("bear");
-
+                // console.log("ITS BEAR TIME");
+                // updateAllTokenUris("bear");
+                currentMarketTrend = MarketTrend.BEAR;
             } else {
                 // bull
-                console.log("ITS BULL TIME");
-                updateAllTokenUris("bull");
+                // console.log("ITS BULL TIME");
+                // updateAllTokenUris("bull");
+                currentMarketTrend = MarketTrend.BULL;
             }
 
+            requestRandomnessForNFTUris();
             // update currentPrice
             currentPrice = latestPrice;
         } else {
@@ -126,11 +146,12 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
             /*uint startedAt*/,
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
-        ) = pricefeed.latestRoundData();
+        ) = priceFeed.latestRoundData();
 
         return price; //  example price returned 3034715771688
     }
   
+    // The logic from this has been moved up to fulfill random words.
     function updateAllTokenUris(string memory trend) internal {
         if (compareStrings("bear", trend)) {
             console.log(" UPDATING TOKEN URIS WITH ", "bear", trend);
@@ -148,13 +169,70 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, KeeperCompatibl
         emit TokensUpdated(trend);
     }
 
-    function setPriceFeed(address newFeed) public onlyOwner {
-        pricefeed = AggregatorV3Interface(newFeed);
+    function requestRandomnessForNFTUris() internal {
+        require(s_subscriptionId != 0, "Subscription ID not set"); 
+
+        // Will revert if subscription is not set and funded.
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyhash,
+            s_subscriptionId, // See https://vrf.chain.link/
+            3, //minimum confirmations before response
+            callbackGasLimit,
+            1 // `numWords` : number of random values we want. Max number for rinkeby is 500 (https://docs.chain.link/docs/vrf-contracts/#rinkeby-testnet)
+        );
+
+        console.log("Request ID: ", s_requestId);
+
+        // requestId looks like uint256: 80023009725525451140349768621743705773526822376835636211719588211198618496446
     }
+
+    // This is the callback that the VRF coordinator sends the 
+    // random values to.
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+        // randomWords looks like this uint256: 68187645017388103597074813724954069904348581739269924188458647203960383435815
+        console.log("---fulfillRandomWords---");
+    
+        string[] memory urisForTrend = currentMarketTrend == MarketTrend.BULL ? bullUrisIpfs : bearUrisIpfs;
+        uint256 idx = randomWords[0] % urisForTrend.length; // use modulo to choose a random index.
+
+
+        for (uint i = 0; i < _tokenIdCounter.current() ; i++) {
+            _setTokenURI(i, urisForTrend[idx]);
+        } 
+
+        string memory trend = currentMarketTrend == MarketTrend.BULL ? "bullish" : "bearish";
+    
+        emit TokensUpdated(trend);
+    }
+
+
+    function setPriceFeed(address newFeed) public onlyOwner {
+        priceFeed = AggregatorV3Interface(newFeed);
+    }
+
     function setInterval(uint256 newInterval) public onlyOwner {
         interval = newInterval;
     }
-    
+
+
+    // For VRF Subscription Manager
+    function setSubscriptionId(uint64 _id) public onlyOwner {
+        console.log("Setting s_subscriptionId");
+        console.log(_id);
+        s_subscriptionId = _id;
+    }
+
+    function setCallbackGasLimit(uint32 maxGas) public onlyOwner {
+      callbackGasLimit = maxGas;
+    }
+
+    function setVrfCoodinator(address _address) public onlyOwner {
+        COORDINATOR = VRFCoordinatorV2Interface(_address);
+    }
 
     function compareStrings(string memory a, string memory b) internal pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
